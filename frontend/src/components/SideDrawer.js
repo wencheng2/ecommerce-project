17d6ef8f12c116f1f9c4c7c20a4aa8bd8b8406d7
wencheng2 <103392823+wencheng2@users.noreply.github.com>
2022-06-24 13:01:34 +1000

    DM-21 Changes to SideDrawer component and linking to Jira

--- frontend/src/components/SideDrawer.js
+++ frontend/src/components/SideDrawer.js
@@ -1,4 +1,5 @@
 import "./SideDrawer.css";
+import {Link} from 'react-router-dom';
 
 const SideDrawer = ({ show, click }) => {
   const sideDrawerClass = ["sidedrawer"];
@@ -7,7 +8,24 @@ const SideDrawer = ({ show, click }) => {
     sideDrawerClass.push("show");
   }
 
-  return  <div className={sideDrawerClass.join("")}>SideDrawer</div>;
+  return (
+    <div className={sideDrawerClass.join(" ")}>
+      <ul className="sidedrawer__links" onClick={click}>
+        <li>
+          <Link to="/cart">
+            <i className="fas fa-shopping-cart"></i>
+            <span>
+              Cart{" "}
+              <span className="sidedrawer__cartbadge">0</span>
+            </span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">Shop</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default SideDrawer;
